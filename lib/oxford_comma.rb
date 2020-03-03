6f0a5c03263164758bea007a87d219c4eba69b40
Christopher Chaison <cchaison0@gmail.com>
2020-03-03 23:46:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.length
+  when 2
+    array[-2] << " and "
+    array.join
+  when 1
+    array.join
+  else
+    array[-1].prepend "and "
+    array.join(", ")
+  end
 end
