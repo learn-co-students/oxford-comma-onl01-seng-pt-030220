258fd0790aafb9d9746733c4f71894a20f8b136f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-05 01:22:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+          array.join(",")
+          array.join(" and ")
+    end
 end
