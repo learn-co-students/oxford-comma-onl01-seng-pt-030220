a6b812d02681da39385afe98be80d183c78cdf51
James Dodds <dodds1990@gmail.com>
2020-03-05 22:00:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_item = array[-1]
+    array.pop
+    output = array.join(", ")
+    output += ", and " + last_item
+    return output
+  end
 end
