ac90f39a52b58082897bcbd8e68ea378e7008c40
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-05 08:27:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,14 @@ def oxford_comma(array)
     elsif array.length == 2
         array.join(" and ")
     elsif array.length == 3
-          array.join(",")
-          array.join(" and ")
+        str=array.join(", ")
+        str[str.length-10] = " and "
+        str
+    else
+      #str=array.join(", ")
+      #str[str.length-14] = " and"
+      #str[str.length-13] = " d"
+      #str
+      "#{array[0..array.length-2].join(", ")}, and #{array[array.length-1]}"
     end
 end
