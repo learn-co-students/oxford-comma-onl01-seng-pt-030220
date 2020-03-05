a7f3328cfe8ec75efe56ad8ebd2121075f603c16
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-05 16:42:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1 
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else 
+    last_element = array.pop 
+    "#{array.join(", ")}, and #{last_element}"
+  end
 end
