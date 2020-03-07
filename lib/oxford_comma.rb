bb127916d04badc3170c810a3bff316485bfe0bb
Megan Christine McClure <mcclme02@luther.edu>
2020-03-07 02:49:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+# write a method for oxford_comma that takes an argument[""] and returns a string with commas and 'and' 
 
+
+def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2 
+    array.join(" and ")
+  else
+    last_element = array.pop
+    array.join(", ")  << ", and " << last_element
+  end 
 end
+
