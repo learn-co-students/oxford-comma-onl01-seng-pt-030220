5eca2d103d8314beb1a7789098d4b771e629dce6
Megan Christine McClure <mcclme02@luther.edu>
2020-03-07 02:58:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,3 +12,16 @@ def oxford_comma(array)
   end 
 end
 
+#method call oxford_comma (argument[" "])
+#method --> oxford comma --> take 1 argument(array of strings) convert those strings into one large string with commas and an 'and' 
+
+# def oxford_comma(array)
+#   if array.count == 1 
+#     array.join
+#   elsif array.count == 2 
+#     array.join(" and ")
+#   else
+#     last_element = array.pop
+#     "#{array.join(", ")}, and #{last_element}"
+#   end
+# end
