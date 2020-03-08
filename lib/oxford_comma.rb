16db0a9388fbb38262af71b9691fc3a95dced0a3
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-08 00:13:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(",")
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  else
+    nil
+  end
 end
