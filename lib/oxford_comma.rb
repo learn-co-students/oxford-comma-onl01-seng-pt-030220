a663690b790d9c672335ec8e6060df57f58261a2
YUKIKO IKEDA <applelele@gmail.com>
2020-03-09 03:33:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+     array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    last_string = array.pop
+    array << "and " + last_string
+    array.join(", ")
+  else
+    last_string = array.pop
+    array << "and " + last_string
+    array.join(", ")
+  end
 end
