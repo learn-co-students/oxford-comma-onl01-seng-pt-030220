c13024c6590d63151654c34b4f4f989ba2b0455f
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-10 00:05:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.size ==1
+  return array[0]
 
+elsif array.size ==2
+  return array.join (" and ")
+
+elsif array.size ==3
+  return array[0..1].join(", ") + ", and " + array[2]
+
+else
+  return array[0..-2].join(", ") + ", and " + array[-1]
+
+end
 end
