645f8ab98850c046babe11172c52e4d252ccba65
Sean Melton <seanlape@gmail.com>
2020-03-10 18:11:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+array = ["apple"]
 def oxford_comma(array)
-
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
