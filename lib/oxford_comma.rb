8cf2aded291566470594d3ef584013dc8f318633
Domenico Cascone <dcascone84@gmail.com>
2020-03-10 18:20:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+#require "pry"
 
+def oxford_comma(array)
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    "#{array[-1].insert(0, "and ")}"
+    array.join(", ")
+  end
 end
