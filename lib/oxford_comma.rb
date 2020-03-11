110e560811bd012f5737ac636460ba5d6ffa7400
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-11 01:26:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
-end
+  if array.size == 1
+  array.join
+  elsif array.size == 2
+    array.join(" and ")
+    elsif array.size == 3
+    array[0..1].join(", ")+", and "+array[2]
+  else array.size > 3
+    array[0..-2].join(", " )+", and "+array[-1]
+    end
+    
+    end
