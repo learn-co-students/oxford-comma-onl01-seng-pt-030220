2dd684d270919c91de94a2fce45792e3ed5369ef
Pablo Menz <p.menz.r@gmail.com>
2020-03-12 01:55:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,31 @@
 def oxford_comma(array)
-
+ str = ""
+ if array.count == 1 
+  return array[0]
+  end
+  
+if array.count == 2 
+  return array[0] + " and " + array[1]
 end
+ 
+ 
+ 
+ array.each_with_index {| n , index | 
+ 
+ if index == array.count - 1 
+ str << ", and " + n 
+ 
+ elsif index == 0
+ 
+ str << n
+ 
+ else
+    
+ str << ", " + n
+ 
+ end
+ }
+ str
+ end
+
+ 
