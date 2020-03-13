3ebfb3537606e7733d7d04ecebc9b9e305209d65
celebrated-strategy-2405 <robertaledo22@gmail.com>
2020-03-13 01:24:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,10 @@
 #Hint: Remember, strings can be operated on very similarly to arrays. For instance, you can add elements to the end of strings with the << ("shovel") method just like you can with arrays.
 #Hint: What methods are available to you for converting arrays into strings? You might want to look it up in the Ruby Documentation.
 #This might be a challenging lab, so take your time using Google and playing around with your code. Good luck and have fun!
+#returns a string without any additional fomatting when given a 1-element array
+  #adds 'and' between elements when given a 2-element array
+ # adds commas plus a final 'and' when given a 3-element array
+  #correctly formats arrays of lengths greater than three
 
 
 def oxford_comma(list)
