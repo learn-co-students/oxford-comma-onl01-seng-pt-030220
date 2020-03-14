02e067eac69296888e17142bbf0fd11f2420b23f
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 01:29:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  
+  
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    last_item = "and #{array[-1]}"
+    array.pop
+    array.push(last_item)
+    return array.join(", ")
+  end
+  
 
 end
