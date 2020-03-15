b4c4fe38d4e2a00c067035eededc512cbefe0dfb
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-15 22:17:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+
+
 def oxford_comma(array)
+
+  if array.size ==1
+    array.join
+  elsif array.size == 2
+    array[0..array.size-2].join(" ")+" and "+array[-1]
+  elsif array.size >= 3
+    array[0..array.size-2].join(", ")+", and "+array[-1]
+  end
 
 end
