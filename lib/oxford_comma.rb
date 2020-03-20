7a1ccb3de0a342276968d8b0ec9672b53ebfe783
Kimberly Aviles <01010110.v@gmail.com>
2020-03-20 03:09:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+def oxford_comma(array_of_string_elements)
+  aose = array_of_string_elements
+
+  case element_counter = aose.length
+  when 1
+    return aose.join
+  when 2
+    return aose.join(" and ")
+  when 3
+    return aose[0..-2].join(", ") + ", and " + aose[-1]
+  else
+    return aose[0..-2].join(", ") + ", and " + aose[-1]
+  end
 
 end
+
+
+  # puts aose[0..-2].join(", ") + " and " + aose[-1]
