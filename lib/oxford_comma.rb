da32402b0d09b505a68cb7f820d7cd2709108299
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-13 13:23:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,12 @@
+
 def oxford_comma(array)
-    if array.length == 1
-        array.join
-    elsif array.length == 2
-        array.join(" and ")
-    elsif array.length == 3
-        str=array.join(", ")
-        str[str.length-10] = " and "
-        str
-    else
-      #str=array.join(", ")
-      #str[str.length-14] = " and"
-      #str[str.length-13] = " d"
-      #str
+
+    if array.length == 2
+          array.join(" and ")
+    elsif  array.length > 2
       "#{array[0..array.length-2].join(", ")}, and #{array[array.length-1]}"
+    else
+      array.join
     end
+  
 end
